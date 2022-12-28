1ac03299931f2090579351891ae61521502c6325
Praveen Samuel <prsamuel1994@gmail.com>
2022-12-28 11:13:31 -0500

    Express router, server logic, controllers for post CRUD operations

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose'
 import PostMessage from '../models/postMessage.js'
 
 export const getPosts = async (req,res) => {
@@ -22,4 +23,37 @@ export const createPosts = async (req, res) => {
     } catch (error) {
         res.status(409).json({ message: error.message })
     }
+}
+
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params
+    const post = req.body
+
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No Post matches provide id')
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {new: true})
+
+    res.json(updatedPost)
+}
+
+export const deletePost = async (req, res) => {
+    const { id } = req.params
+    
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No Post matches provide id')
+
+    await PostMessage.findByIdAndRemove(id)
+
+    res.json({message: 'Post deleted successfully'})
+
+}
+
+export const likePost = async (req, res) => {
+    const { id } = req.params
+    
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No Post matches provide id')
+    
+    const post = await PostMessage.findById(id)
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, {likeCount: post.likeCount +1}, {new: true})
+
+    res.json(updatedPost)
 }
--- index.js
+++ index.js
@@ -2,8 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv'
 
 import postRoutes from './routes/posts.js'
+
+dotenv.config()
 
 const app = express();
 
@@ -14,7 +17,7 @@ app.use(cors());
 app.use('/posts', postRoutes)
 
 
-const CONNECTION_URL = 'mongodb+srv://SamuelPJ:6rQNpAthbSQEPsA@cluster0.c2qvsh9.mongodb.net/?retryWrites=true&w=majority'
+const CONNECTION_URL = process.env.CONNECTION_URL
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
