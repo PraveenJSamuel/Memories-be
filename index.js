9d1fdf13ae86b31b3198f0723c1e92a7cc7679b6
Praveen Samuel <prsamuel1994@gmail.com>
2022-12-31 19:56:44 -0500

    Final adjustments to auth and routing / ready for deploy

--- index.js
+++ index.js
@@ -3,23 +3,26 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv'
-
+import userRouter from './routes/user.js'
 import postRoutes from './routes/posts.js'
 
 dotenv.config()
 
 const app = express();
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-app.use('/posts', postRoutes)
+app.use('/posts', postRoutes);
+app.use("/user", userRouter);
 
 
 const CONNECTION_URL = process.env.CONNECTION_URL
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-    .catch((error) => console.log(error.message))
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false)
--- routes/posts.js
+++ routes/posts.js
@@ -1,13 +1,19 @@
 import express from 'express';
 
-import { getPosts, createPosts, updatePost, deletePost, likePost } from '../controllers/posts.js';
+import { getPosts, getPostsBySearch, getPostsByCreator, getPost, createPost, updatePost, likePost, commentPost, deletePost } from '../controllers/posts.js';
 
-const router = express.Router()
+const router = express.Router();
+import auth from "../middleware/auth.js";
 
-router.get('/', getPosts)
-router.post('/', createPosts)
-router.patch('/:id', updatePost)
-router.delete('/:id', deletePost)
-router.patch('/:id/likePost', likePost)
+router.get('/creator', getPostsByCreator);
+router.get('/search', getPostsBySearch);
+router.get('/', getPosts);
+router.get('/:id', getPost);
 
-export default router
+router.post('/', auth,  createPost);
+router.patch('/:id', auth, updatePost);
+router.delete('/:id', auth, deletePost);
+router.patch('/:id/likePost', auth, likePost);
+router.post('/:id/commentPost', commentPost);
+
+export default router;
